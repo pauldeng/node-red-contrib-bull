f924b6988c213e54a6334e2843f8415e3d274a9d
Paul Deng <dengpeng.cn@gmail.com>
2019-06-21 09:47:39 +1000

    prettier applied

--- bull-queue.js
+++ bull-queue.js
@@ -15,186 +15,197 @@
  **/
 
 module.exports = function(RED) {
-	"use strict";
-	var q = require('q');
-	var Queue = require("bull");
+  "use strict";
+  var q = require("q");
+  var Queue = require("bull");
 
-	function BullQueueServerSetup(config) {
-		RED.nodes.createNode(this, config);
+  function BullQueueServerSetup(config) {
+    RED.nodes.createNode(this, config);
 
-		this.connected = false;
-		this.connecting = false;
-		this.usecount = 0;
-		// Config node state
-		this.name = config.name;
-		this.address = config.address;
-		this.port = config.port;
+    this.connected = false;
+    this.connecting = false;
+    this.usecount = 0;
+    // Config node state
+    this.name = config.name;
+    this.address = config.address;
+    this.port = config.port;
 
-		var node = this;
-		this.register = function() {
-			node.usecount += 1;
-		};
+    var node = this;
+    this.register = function() {
+      node.usecount += 1;
+    };
 
-		this.deregister = function() {
-			node.usecount -= 1;
-			if (node.usecount == 0) {
-			}
-		};
+    this.deregister = function() {
+      node.usecount -= 1;
+      if (node.usecount == 0) {
+      }
+    };
 
-		this.connect = function() {
-			var deferred = q.defer();
-			if (!node.connected && !node.connecting) {
-				node.connecting = true;
-				node.queue = Queue(node.name, node.port, node.address);
-				node.log("connected", {
-					server : (node.address ? node.address + "@" : "") + node.port
-				});
-				node.connecting = false;
-				node.connected = true;
-				node.emit('connected');
-				deferred.resolve(node.queue);
-			} else {
-				if (node.queue) {
-					deferred.resolve(node.queue);
-				}
-			}
-			return deferred.promise;
-		};
+    this.connect = function() {
+      var deferred = q.defer();
+      if (!node.connected && !node.connecting) {
+        node.connecting = true;
+        node.queue = Queue(node.name, node.port, node.address);
+        node.log("connected", {
+          server: (node.address ? node.address + "@" : "") + node.port
+        });
+        node.connecting = false;
+        node.connected = true;
+        node.emit("connected");
+        deferred.resolve(node.queue);
+      } else {
+        if (node.queue) {
+          deferred.resolve(node.queue);
+        }
+      }
+      return deferred.promise;
+    };
 
-		this.on('close', function(removed, closecomplete) {
-			if (removed){
-				// This node has been deleted
-			} else {
-				// This node is being restarted
-			}
-			node.Queue.close();
-			node.connecting = false;
-			node.connected = false;
-			node.log("closed");
-			closecomplete()
-		});
-	}
+    this.on("close", function(removed, closecomplete) {
+      if (removed) {
+        // This node has been deleted
+      } else {
+        // This node is being restarted
+      }
+      node.Queue.close();
+      node.connecting = false;
+      node.connected = false;
+      node.log("closed");
+      closecomplete();
+    });
+  }
 
+  RED.nodes.registerType("bull-queue-server", BullQueueServerSetup);
 
-	RED.nodes.registerType("bull-queue-server", BullQueueServerSetup);
+  function BullQueueCmdNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    this.name = config.name;
+    this.queue = config.queue;
+    this.cmd = config.cmd;
+    this.Queue = RED.nodes.getNode(this.queue);
+    if (node.Queue) {
+      node.Queue.register();
+      node.Queue.connect().then(
+        function(queue) {
+          node.status({
+            fill: "green",
+            shape: "dot",
+            text: "connected"
+          });
+        },
+        function(error) {
+          node.status({
+            fill: "red",
+            shape: "ring",
+            text: "disconnected"
+          });
+        }
+      );
+    } else {
+      node.error("common.status.error");
+    }
+    try {
+      this.on("input", function(msg) {
+        node.Queue.connect().then(
+          function(queue) {
+            switch (parseInt(node.cmd)) {
+              case 0:
+                node.log(
+                  "queue.add({payload:" +
+                    msg.payload +
+                    "}, " +
+                    JSON.stringify(msg.jobopts) +
+                    ")"
+                );
+                async function add(msg) {
+                  return await queue.add({ payload: msg.payload }, msg.jobopts);
+                }
+                msg.result = add(msg);
+                node.send(msg);
+                break;
+              case 1:
+                node.log("queue.pause()", node.cmd);
+                queue.pause();
+                break;
+              case 2:
+                node.log("queue.resume()", node.cmd);
+                queue.resume();
+                break;
+              default:
+                node.log("queue.default()", node.cmd);
+                break;
+            }
+          },
+          function(error) {
+            node.status({
+              fill: "red",
+              shape: "ring",
+              text: "disconnected"
+            });
+          }
+        );
+      });
+    } catch (err) {
+      // eg SyntaxError - which v8 doesn't include line number information
+      // so we can't do better than this
+      this.error(err);
+    }
 
-	function BullQueueCmdNode(config) {
-		RED.nodes.createNode(this, config);
-		var node = this;
-		this.name = config.name;
-		this.queue = config.queue;
-		this.cmd = config.cmd;
-		this.Queue = RED.nodes.getNode(this.queue);
-		if (node.Queue) {
-			node.Queue.register();
-			node.Queue.connect().then(function(queue) {
-				node.status({
-					fill : "green",
-					shape : "dot",
-					text : "connected"
-				});
-			}, function(error) {
-				node.status({
-					fill : "red",
-					shape : "ring",
-					text : "disconnected"
-				});
-			});
-		} else {
-			node.error("common.status.error");
-		}
-		try {
-			this.on("input", function(msg) {
-				node.Queue.connect().then(function(queue) {
-					switch (parseInt(node.cmd)) {
-					case 0:
-						node.log("queue.add({payload:" + msg.payload + "}, " + JSON.stringify(msg.jobopts) + ")");
-						async function add(msg){
-							return await queue.add(
-								{payload: msg.payload},
-								msg.jobopts);
-						}
-						msg.result = add(msg);
-						node.send(msg);
-						break;
-					case 1:
-						node.log("queue.pause()", node.cmd);
-						queue.pause();
-						break;
-					case 2:
-						node.log("queue.resume()", node.cmd);
-						queue.resume();
-						break;
-					default:
-						node.log("queue.default()", node.cmd);
-						break;
-					}
-				}, function(error) {
-					node.status({
-						fill : "red",
-						shape : "ring",
-						text : "disconnected"
-					});
-				});
-			});
-		} catch(err) {
-			// eg SyntaxError - which v8 doesn't include line number information
-			// so we can't do better than this
-			this.error(err);
-		};
+    this.on("close", function(removed, done) {
+      if (removed) {
+        // This node has been deleted
+      } else {
+        // This node is being restarted
+      }
+      node.Queue.close();
+      done();
+    });
+  }
 
-		this.on('close', function(removed, done) {
-			if (removed) {
-				// This node has been deleted
-			} else {
-				// This node is being restarted
-			}
-			node.Queue.close();
-			done();
-		});
-	}
+  function BullQueueRunNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    this.queue = config.queue;
+    this.Queue = RED.nodes.getNode(this.queue);
+    if (node.Queue) {
+      node.Queue.register();
+      node.Queue.connect().then(
+        function(queue) {
+          node.status({
+            fill: "green",
+            shape: "dot",
+            text: "connected"
+          });
+          queue.process(function(job, completed) {
+            node.log(JSON.stringify(job));
+            node.send(job.data);
+            completed();
+          });
+        },
+        function(error) {
+          node.status({
+            fill: "red",
+            shape: "ring",
+            text: "disconnected"
+          });
+        }
+      );
+    } else {
+      node.error("common.status.error");
+    }
 
-	function BullQueueRunNode(config) {
-		RED.nodes.createNode(this, config);
-		var node = this;
-		this.queue = config.queue;
-		this.Queue = RED.nodes.getNode(this.queue);
-		if (node.Queue) {
-			node.Queue.register();
-			node.Queue.connect().then(function(queue) {
-				node.status({
-					fill : "green",
-					shape : "dot",
-					text : "connected"
-				});
-				queue.process(function(job, completed) {
-					node.log(JSON.stringify(job));
-					node.send(job.data);
-					completed();
-				});
-			}, function(error) {
-				node.status({
-					fill : "red",
-					shape : "ring",
-					text : "disconnected"
-				});
-			});
-		} else {
-			node.error("common.status.error");
-		};
+    this.on("close", function(removed, done) {
+      if (removed) {
+        // This node has been deleted
+      } else {
+        // This node is being restarted
+      }
+      node.Queue.close();
+      done();
+    });
+  }
 
-		this.on('close', function(removed, done) {
-			if (removed) {
-				// This node has been deleted
-			} else {
-				// This node is being restarted
-			}
-			node.Queue.close();
-			done();
-		});
-	}
-
-
-	RED.nodes.registerType("bull cmd", BullQueueCmdNode);
-	RED.nodes.registerType("bull run", BullQueueRunNode);
+  RED.nodes.registerType("bull cmd", BullQueueCmdNode);
+  RED.nodes.registerType("bull run", BullQueueRunNode);
 };
